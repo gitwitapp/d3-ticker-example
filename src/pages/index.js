73d3b233272fdfe94cbd887fc7be9f95bf6cd2cd
Gitwit <git@gitwit.dev>
2023-07-05 23:12:37 +0000

    Add code to create a line chart using d3 library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,1 @@
-import Head from "next/head";
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-      </Head>
-      <main>
-        <div>
-          hello, world!
-        </div>
-      </main>
-    </>
-  );
-}
+Add code to create a line chart using d3 library
