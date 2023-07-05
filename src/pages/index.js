8aee53c34b2023e3758e2bdf5e7d782f93eca5a3
Gitwit <git@gitwit.dev>
2023-07-05 23:20:13 +0000

    Update index.js to fetch Bitcoin price data from Binance API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-
 import Head from "next/head";
 import { useEffect, useRef } from "react";
 import * as d3 from "d3";
@@ -7,53 +6,57 @@ export default function Home() {
   const chartRef = useRef(null);
 
   useEffect(() => {
-    const data = [
-      { date: new Date("2022-01-01"), price: 30000 },
-      { date: new Date("2022-01-02"), price: 32000 },
-      { date: new Date("2022-01-03"), price: 31000 },
-      // Add more data points here
-    ];
+    const fetchData = async () => {
+      const response = await fetch("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1d&startTime=1640995200000");
+      const data = await response.json();
+      const formattedData = data.map((d) => ({
+        date: new Date(d[0]),
+        price: parseFloat(d[4])
+      }));
 
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const width = 800 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+      const width = 800 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
 
-    const svg = d3
-      .select(chartRef.current)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      const svg = d3
+        .select(chartRef.current)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    const x = d3
-      .scaleTime()
-      .domain(d3.extent(data, (d) => d.date))
-      .range([0, width]);
+      const x = d3
+        .scaleTime()
+        .domain(d3.extent(formattedData, (d) => d.date))
+        .range([0, width]);
 
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.price)])
-      .range([height, 0]);
+      const y = d3
+        .scaleLinear()
+        .domain([0, d3.max(formattedData, (d) => d.price)])
+        .range([height, 0]);
 
-    const line = d3
-      .line()
-      .x((d) => x(d.date))
-      .y((d) => y(d.price));
+      const line = d3
+        .line()
+        .x((d) => x(d.date))
+        .y((d) => y(d.price));
 
-    svg
-      .append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
+      svg
+        .append("path")
+        .datum(formattedData)
+        .attr("class", "line")
+        .attr("d", line);
 
-    svg
-      .append("g")
-      .attr("class", "x-axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      svg
+        .append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-    svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
+      svg.append("g").attr("class", "y-axis").call(d3.axisLeft(y));
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -67,4 +70,3 @@ export default function Home() {
     </>
   );
 }
-
